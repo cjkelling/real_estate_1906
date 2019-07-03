1d2833365b5f20e044e2e232f6fbf814bede3b5d
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-03 10:17:15 -0600

    Finish iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -5,10 +5,24 @@ class House
     @price = price
     @address = address
     @rooms = []
+    @total_area = 0
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    @rooms.each do |room|
+      @total_area += room.area
+    end
+    @total_area
   end
 
 end
