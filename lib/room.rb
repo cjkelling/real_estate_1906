37caab88068ff9c76749c8998ae2485450af1b9a
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-03 09:22:54 -0600

    Finish iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,9 @@
 class Room
+  attr_reader :category, :area
+
+  def initialize(category, x, y)
+    @category = category
+    @area = x * y
+  end
 
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -4,4 +4,17 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
 
+  def test_room_exists
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_instance_of Room, room
+  end
+
+  def test_attributes
+    room = Room.new(:bedroom, 10, 13)
+
+    assert_equal :bedroom, room.category
+    assert_equal 130, room.area
+  end
+
 end
